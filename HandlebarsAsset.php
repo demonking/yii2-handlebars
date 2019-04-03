9ad38a58f51102822b710098c67d05b9ce09aaa8
Denis Warsow <denis.warsow@xisio.com>
2019-04-03 10:15:15 +0200

    fxp-asset-manager are deprecated in yii2

--- HandlebarsAsset.php
+++ HandlebarsAsset.php
@@ -3,7 +3,7 @@ namespace x1\handlebars;
 
 class HandlebarsAsset extends \yii\web\AssetBundle
 {
-	public $sourcePath = '@vendor/bower/handlebars';
+	public $sourcePath = '@bower/handlebars';
 	
 	public $js         = [
 		'handlebars.js' => 'handlebars.min.js',
